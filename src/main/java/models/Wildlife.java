58453dd384dd3db12698e8d4789cda669a10b921
wil-bert <ianwilbuts@gmail.com>
2020-09-20 16:16:27 +0300

    initial page

--- src/main/java/models/Wildlife.java
+++ src/main/java/models/Wildlife.java
@@ -1,4 +1,46 @@
 package models;
 
-public class Wildlife {
+import org.sql2o.Connection;
+import java.util.Objects;
+
+public abstract class Wildlife {
+
+    public int id;
+    public String name;
+    public String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() { return type; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Wildlife wildlife = (Wildlife) o;
+        return Objects.equals(name, wildlife.name) &&
+                Objects.equals(type, wildlife.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type);
+    }
+
+    public void save(){
+        try (Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO animals(name, type) VALUES(:name,:type)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name",this.name)
+                    .addParameter("type",this.type)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
 }
--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,7 +1,60 @@
 package models;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void animal_instantiateCorrectly_true(){
+        Animal testAnimal = setUpAnimal();
+        assertTrue(testAnimal instanceof Animal);
+    }
+
+    @Test
+    public void animal_instantiateWithName_String(){
+        Animal testAnimal = setUpAnimal();
+        assertEquals("lion", testAnimal.getName());
+    }
+    @Test
+    public void animal_instantiateWithCorrectType_String(){
+        Animal testAnimal = setUpAnimal();
+        assertEquals("animal", testAnimal.getType());
+    }
+    @Test
+    public void equals_returnTrueWhenObjectsHaveSameValues_true(){
+        Animal testAnimal = setUpAnimal();
+        Animal testAnimal2 = setUpAnimal();
+        assertTrue(testAnimal.equals(testAnimal2));
+    }
+    @Test
+    public void save_addAnimalToDatabase_void(){
+        Animal testAnimal = setUpAnimal();
+        testAnimal.save();
+        assertTrue(Animal.all().get(0).equals(testAnimal));
+    }
+    @Test
+    public void all_returnAllAnimals_List(){
+        Animal testAnimal = setUpAnimal();
+        testAnimal.save();
+        Animal testAnimal2 = setUpAnimal();
+        testAnimal2.save();
+        assertEquals(2,Animal.all().size());
+    }
+
+    @Test
+    public void animal_instantiateWithCorrectId_int(){
+
+    }
+
+    //helper class
+    public Animal setUpAnimal(){
+        return new Animal("lion");
+    }
 
 }
