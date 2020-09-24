fd909896e3fc2e7e3cfac3a62a08f79dd8df35e4
wil-bert <ianwilbuts@gmail.com>
2020-09-24 07:43:12 +0300

    refactore code

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -3,8 +3,7 @@ package models;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class AnimalTest {
 
